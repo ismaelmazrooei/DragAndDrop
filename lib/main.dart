b5a3a99da4aacb9d073f080b85193b7a03cadf46
ismaelmazrooei <ismaelmazrooei@gmail.com>
2022-11-09 11:53:25 +0330

    update

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'جلیل',
+      title: 'ismael',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
